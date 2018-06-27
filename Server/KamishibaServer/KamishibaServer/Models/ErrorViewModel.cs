ff20b80ade3ca1b411f1b42693a07f6673d2cd08
wallstudio <yukawallstudio@gmail.com>
2018-06-28 06:02:11 +0900

    KamishibaServerプロジェクトの生成

---
+++ Server/KamishibaServer/KamishibaServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace KamishibaServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
