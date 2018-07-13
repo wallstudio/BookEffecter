f841014930655895378de7ac6eb8b7e74f8e65dc
wallstudio <yukawallstudio@gmail.com>
2018-07-14 02:18:13 +0900

    スイッチの状態を保存

--- Android/Kamishiba/app/src/main/java/wallstudio/work/kamishiba/LauncherActivity.java
+++ Android/Kamishiba/app/src/main/java/wallstudio/work/kamishiba/LauncherActivity.java
@@ -2,7 +2,9 @@ package wallstudio.work.kamishiba;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -21,6 +23,7 @@ import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.opencv.core.Mat;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
@@ -89,6 +92,17 @@ public class LauncherActivity extends AppCompatActivity {
             // Show the Up button in the action bar.
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        try {
+            String packageConfigPath = getFilesDir() + "/" + mPackageId + "/" + LoadUtil.PACKAGE_CONFIG_FILENAME;
+            if (!new File(packageConfigPath).exists()) {
+                LoadUtil.preferPackageConfigPath(packageConfigPath);
+            }
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+            int common = Integer.parseInt(sharedPreferences.getString("pref_default_cam", "0"));
+            int pack = Integer.parseInt(((Map<String, String>)LoadUtil.getYamlFromPath(packageConfigPath)).get("camera"));
+            mCameraSwitch.setChecked(pack < 0 ? common == 0 : pack == 0);
+        }catch (Exception e){}
     }
 
     @Override
@@ -195,6 +209,15 @@ public class LauncherActivity extends AppCompatActivity {
         setIsDownloaded(false);
         Toast.makeText(this, "Deleted " + mPackageId, Toast.LENGTH_SHORT).show();
         Log.d("Launcher", "DELETE " + mPackageId);
+    }
+
+    public void onClickCameraSwitch(View view){
+        try {
+            String packageConfigPath = getFilesDir() + "/" + mPackageId + "/" + LoadUtil.PACKAGE_CONFIG_FILENAME;
+            Map<String, String> pack = (Map<String, String>)LoadUtil.getYamlFromPath(packageConfigPath);
+            pack.put("camera", ((Switch)view).isChecked() ? "0": "1");
+            LoadUtil.saveString(new Yaml().dump(pack), packageConfigPath);
+        }catch (Exception e){}
     }
 
     public static class AudioAdapter extends ArrayAdapter<Map> {
